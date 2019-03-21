192a4584c3bd554e83c84dfa6613a7d66a428747
bavrahami00 <bavrahami00@stuy.edu>
2019-03-21 14:43:18 -0400

    Constructor,size,toString completed

--- MyDeque.java
+++ MyDeque.java
@@ -4,16 +4,29 @@ public class MyDeque<E> {
   private int start;
   private int end;
   public MyDeque(){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+    start = 0;
+    end = 0;
   }
   public MyDeque(int initialCapacity){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+    start = 0;
+    end = 0;
   }
   public int size(){
-
+    return (data.length+end-start)%data.length+1;
   }
   public String toString(){
-
+    String ans = "{";
+    for (int x = 0; x < size; x++) {
+      ans += data[(x+start)%data.length] + " ";
+    }
+    ans += "}";
+    return ans;
   }
   public void addFirst(E element){
 
@@ -31,6 +44,6 @@ public class MyDeque<E> {
 
   }
   public E getLast(){
-    
+
   }
 }
