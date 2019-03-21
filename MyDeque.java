fb6426984ce9ba59b0f35ab2291228875643936c
bavrahami00 <bavrahami00@stuy.edu>
2019-03-21 14:53:10 -0400

    AddFirst and AddLast completed

--- MyDeque.java
+++ MyDeque.java
@@ -29,10 +29,28 @@ public class MyDeque<E> {
     return ans;
   }
   public void addFirst(E element){
-
+    if (size == data.length) {
+      int[] temp = new int[2*data.length+1];
+      for (int x = 0; x < data.length; x++) {
+        temp[x+1] = data[(x+start)%data.length];
+      }
+      start = 1;
+      end = data.length;
+      data = temp;
+    }
+    data[(start+data.length-1)%data.length] = element;
   }
   public void addLast(E element){
-
+    if (size == data.length) {
+      int[] temp = new int[2*data.length+1];
+      for (int x = 0; x < data.length; x++) {
+        temp[x] = data[(x+start)%data.length];
+      }
+      start = 0;
+      end = data.length;
+      data = temp;
+    }
+    data[(start+1)%data.length] = element;
   }
   public E removeFirst(){
 
