c4bc47fffbf8660018ed20d1c72b0c216b849cbb
bavrahami00 <bavrahami00@stuy.edu>
2019-03-26 14:23:05 -0400

    Eliminated edit

--- Calculator.java
+++ Calculator.java
@@ -2,40 +2,36 @@ public class Calculator {
   public static double eval(String s) {
     MyDeque<Double> stack = new MyDeque<Double>();
     String t = ""+s.charAt(0);
-    for (int x = 1; x < s.length(); x++) {
-      if (s.charAt(x) != ' ') {
-        t += s.charAt(x);
-      }
-      else {
-        edit(t,stack);
+    for (int x = 1; x <= s.length(); x++) {
+      if (x == s.length() || s.charAt(x) == ' ') {
+        try {
+          stack.addLast(Double.parseDouble(t));
+        }
+        catch (NumberFormatException n) {
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          if (t.equals("+")) {
+            stack.addLast(first+second);
+          }
+          else if (t.equals("*")) {
+            stack.addLast(first*second);
+          }
+          else if (t.equals("-")) {
+            stack.addLast(first-second);
+          }
+          else if (t.equals("/")) {
+            stack.addLast(first+second);
+          }
+          else if (t.equals("%")) {
+            stack.addLast(first%second);
+          }
+        }
         t = "";
       }
+      else {
+        t += s.charAt(x);
+      }
     }
-    edit(t,stack);
     return stack.getFirst();
-  }
-  private static void edit(String t, MyDeque<Double> stack) {
-    try {
-      stack.addLast(Double.parseDouble(t));
-    }
-    catch (NumberFormatException n) {
-      double second = stack.removeLast();
-      double first = stack.removeLast();
-      if (t.equals("+")) {
-        stack.addLast(first+second);
-      }
-      else if (t.equals("*")) {
-        stack.addLast(first*second);
-      }
-      else if (t.equals("-")) {
-        stack.addLast(first-second);
-      }
-      else if (t.equals("/")) {
-        stack.addLast(first+second);
-      }
-      else if (t.equals("%")) {
-        stack.addLast(first%second);
-      }
-    }
   }
 }
